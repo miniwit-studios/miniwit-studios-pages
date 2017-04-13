818f22bcc76785ab0543ae30c0f8fa1f4c385bd7
aboveyou00 <brandonyoyoslade@gmail.com>
2017-04-12 21:27:41 -0600

    feat(typescript): upgrade to typescript@2.3.0 RC; add sample async/await in homepage

--- src/modules/landing/app/pages/home/home.ts
+++ src/modules/landing/app/pages/home/home.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { ComponentBase } from 'shared/util';
 
+const DELAY_MILLIS = 1000;
+
 @Component({
     selector: 'home',
     styleUrls: ['./home.scss'],
@@ -10,5 +12,23 @@ import { ComponentBase } from 'shared/util';
 export class HomeComponent extends ComponentBase {
     constructor() {
         super();
+    }
+    
+    private delay(time: number): Promise<void> {
+        return new Promise<void>((resolve, reject) => {
+            setTimeout(() => resolve(), time);
+        });
+    }
+    
+    asyncTaskResult: string = '';
+    runningAsyncTask: boolean = false;
+    async startAsyncTask() {
+        if (this.runningAsyncTask) return;
+        this.runningAsyncTask = true;
+        
+        await this.delay(DELAY_MILLIS);
+        
+        this.asyncTaskResult = 'Everything worked great!';
+        this.runningAsyncTask = false;
     }
 }
