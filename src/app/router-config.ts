626cc8278c0780150bc32269c77c4ba004035485
aboveyou00 <brandonyoyoslade@gmail.com>
2017-04-28 16:57:53 -0600

    feat(landing): remove 'home' path segment from the homepage

--- src/app/router-config.ts
+++ src/app/router-config.ts
@@ -6,8 +6,7 @@ import { ContactComponent } from './pages/contact/contact';
 import { NotFoundComponent } from './pages/not-found/not-found';
 
 export const routerConfig: Routes = [
-    {path: '', redirectTo: 'home', pathMatch: 'full'},
-    {path: 'home', component: HomeComponent},
+    {path: '', component: HomeComponent, pathMatch: 'full'},
     {path: 'about', component: AboutComponent},
     {path: 'contact', component: ContactComponent},
     {path: 'github', loadChildren: '../modules/+github/github.module#GithubModule'},
